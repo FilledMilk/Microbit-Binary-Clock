fa8c31630719c6da10b3a209f090a52d40746b4b
FilledMilk <66450589+FilledMilk@users.noreply.github.com>
2022-09-25 06:16:38 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,383 @@
+function showTenMinute () {
+    if (tenMinuteTime == 0) {
+        tenMinute.clear()
+        tenMinute.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+        tenMinute.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+        tenMinute.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        tenMinute.show()
+    } else if (tenMinuteTime == 1) {
+        tenMinute.clear()
+        tenMinute.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+        tenMinute.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+        tenMinute.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        tenMinute.show()
+    } else if (tenMinuteTime == 2) {
+        tenMinute.clear()
+        tenMinute.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+        tenMinute.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        tenMinute.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        tenMinute.show()
+    } else if (tenMinuteTime == 3) {
+        tenMinute.clear()
+        tenMinute.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+        tenMinute.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        tenMinute.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        tenMinute.show()
+    } else if (tenMinuteTime == 4) {
+        tenMinute.clear()
+        tenMinute.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        tenMinute.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+        tenMinute.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        tenMinute.show()
+    } else if (tenMinuteTime == 5) {
+        tenMinute.clear()
+        tenMinute.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        tenMinute.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        tenMinute.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        tenMinute.show()
+    }
+}
+// Done
+function showUnitMinute () {
+    if (unitMinuteTime == 0) {
+        unitMinute.clear()
+        unitMinute.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+        unitMinute.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+        unitMinute.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        unitMinute.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        unitMinute.show()
+    } else if (unitMinuteTime == 1) {
+        unitMinute.clear()
+        unitMinute.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        unitMinute.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+        unitMinute.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        unitMinute.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        unitMinute.show()
+    } else if (unitHourTime == 2) {
+        unitMinute.clear()
+        unitMinute.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+        unitMinute.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        unitMinute.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        unitMinute.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        unitMinute.show()
+    } else if (unitMinuteTime == 3) {
+        unitMinute.clear()
+        unitMinute.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        unitMinute.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        unitMinute.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        unitMinute.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        unitMinute.show()
+    } else if (unitMinuteTime == 4) {
+        unitMinute.clear()
+        unitMinute.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+        unitMinute.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+        unitMinute.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        unitMinute.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        unitMinute.show()
+    } else if (unitMinuteTime == 5) {
+        unitMinute.clear()
+        unitMinute.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        unitMinute.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+        unitMinute.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        unitMinute.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        unitMinute.show()
+    } else if (unitMinuteTime == 6) {
+        unitMinute.clear()
+        unitMinute.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+        unitMinute.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        unitMinute.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        unitMinute.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        unitMinute.show()
+    } else if (unitMinuteTime == 7) {
+        unitMinute.clear()
+        unitMinute.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        unitMinute.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        unitMinute.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        unitMinute.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        unitMinute.show()
+    } else if (unitMinuteTime == 8) {
+        unitMinute.clear()
+        unitMinute.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+        unitMinute.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+        unitMinute.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        unitMinute.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+        unitMinute.show()
+    } else if (unitMinuteTime == 9) {
+        unitMinute.clear()
+        unitMinute.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        unitMinute.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+        unitMinute.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        unitMinute.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+        unitMinute.show()
+    }
+}
+function getUnitSecondTime () {
+    unitSecondTime = DS3231.second() % 10
+}
+// Done
+function showUnitSecond () {
+    if (unitSecondTime == 0) {
+        unitSecond.clear()
+        unitSecond.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+        unitSecond.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+        unitSecond.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        unitSecond.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        unitSecond.show()
+    } else if (unitSecondTime == 1) {
+        unitSecond.clear()
+        unitSecond.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        unitSecond.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+        unitSecond.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        unitSecond.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        unitSecond.show()
+    } else if (unitSecondTime == 2) {
+        unitSecond.clear()
+        unitSecond.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+        unitSecond.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        unitSecond.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        unitSecond.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        unitSecond.show()
+    } else if (unitSecondTime == 3) {
+        unitSecond.clear()
+        unitSecond.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        unitSecond.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        unitSecond.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        unitSecond.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        unitSecond.show()
+    } else if (unitSecondTime == 4) {
+        unitSecond.clear()
+        unitSecond.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+        unitSecond.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+        unitSecond.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        unitSecond.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        unitSecond.show()
+    } else if (unitSecondTime == 5) {
+        unitSecond.clear()
+        unitSecond.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        unitSecond.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+        unitSecond.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        unitSecond.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        unitSecond.show()
+    } else if (unitSecondTime == 6) {
+        unitSecond.clear()
+        unitSecond.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+        unitSecond.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        unitSecond.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        unitSecond.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        unitSecond.show()
+    } else if (unitSecondTime == 7) {
+        unitSecond.clear()
+        unitSecond.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        unitSecond.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        unitSecond.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        unitSecond.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        unitSecond.show()
+    } else if (unitSecondTime == 8) {
+        unitSecond.clear()
+        unitSecond.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+        unitSecond.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+        unitSecond.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        unitSecond.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+        unitSecond.show()
+    } else if (unitSecondTime == 9) {
+        unitSecond.clear()
+        unitSecond.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        unitSecond.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+        unitSecond.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        unitSecond.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+        unitSecond.show()
+    }
+}
+function getTenSecondTime () {
+    tenSecondTime = Math.trunc(DS3231.second() / 10)
+}
+function getTenMinuteTime () {
+    tenMinuteTime = Math.trunc(DS3231.minute() / 10)
+}
+function getTenHourTime () {
+    tenHourTime = Math.trunc(DS3231.hour() / 10)
+}
+// Done
+function showTenSecond () {
+    if (tenSecondTime == 0) {
+        tenSecond.clear()
+        tenSecond.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+        tenSecond.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+        tenSecond.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        tenSecond.show()
+    } else if (tenSecondTime == 1) {
+        tenSecond.clear()
+        tenSecond.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        tenSecond.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+        tenSecond.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+        tenSecond.show()
+    } else if (tenSecondTime == 2) {
+        tenSecond.clear()
+        tenSecond.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        tenSecond.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        tenSecond.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+        tenSecond.show()
+    } else if (tenSecondTime == 3) {
+        tenSecond.clear()
+        tenSecond.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        tenSecond.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        tenSecond.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+        tenSecond.show()
+    } else if (tenSecondTime == 4) {
+        tenSecond.clear()
+        tenSecond.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        tenSecond.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+        tenSecond.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        tenSecond.show()
+    } else if (tenSecondTime == 5) {
+        tenSecond.clear()
+        tenSecond.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        tenSecond.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+        tenSecond.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        tenSecond.show()
+    }
+}
+function getUnitMinuteTime () {
+    unitMinuteTime = DS3231.minute() % 10
+}
+function getUnitHourTime () {
+    if (DS3231.hour() < 10) {
+        unitHourTime = DS3231.hour()
+    } else if (DS3231.hour() >= 10) {
+        unitHourTime = DS3231.hour() % 10
+    }
+}
+function showUnitHour () {
+    if (unitHourTime == 0) {
+        unitHour.clear()
+        unitHour.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+        unitHour.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+        unitHour.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        unitHour.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        unitHour.show()
+    } else if (unitHourTime == 1) {
+        unitHour.clear()
+        unitHour.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        unitHour.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+        unitHour.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        unitHour.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        unitHour.show()
+    } else if (unitHourTime == 2) {
+        unitHour.clear()
+        unitHour.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+        unitHour.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        unitHour.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        unitHour.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        unitHour.show()
+    } else if (unitHourTime == 3) {
+        unitHour.clear()
+        unitHour.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        unitHour.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        unitHour.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        unitHour.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        unitHour.show()
+    } else if (unitHourTime == 4) {
+        unitHour.clear()
+        unitHour.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+        unitHour.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+        unitHour.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        unitHour.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        unitHour.show()
+    } else if (unitHourTime == 5) {
+        unitHour.clear()
+        unitHour.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        unitHour.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+        unitHour.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        unitHour.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        unitHour.show()
+    } else if (unitHourTime == 6) {
+        unitHour.clear()
+        unitHour.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+        unitHour.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        unitHour.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        unitHour.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        unitHour.show()
+    } else if (unitHourTime == 7) {
+        unitHour.clear()
+        unitHour.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        unitHour.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        unitHour.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        unitHour.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        unitHour.show()
+    } else if (unitHourTime == 8) {
+        unitHour.clear()
+        unitHour.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+        unitHour.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+        unitHour.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        unitHour.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+        unitHour.show()
+    } else if (unitHourTime == 9) {
+        unitHour.clear()
+        unitHour.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        unitHour.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+        unitHour.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        unitHour.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+        unitHour.show()
+    }
+}
+function showTenHour () {
+    if (tenHourTime == 2) {
+        tenHour.clear()
+        tenHour.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        tenHour.show()
+    } else if (tenHourTime == 1) {
+        tenHour.clear()
+        tenHour.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        tenHour.show()
+    } else if (tenHourTime == 0) {
+        tenHour.clear()
+        tenHour.show()
+    }
+}
+let tenHourTime = 0
+let tenSecondTime = 0
+let unitSecondTime = 0
+let unitHourTime = 0
+let unitMinuteTime = 0
+let tenMinuteTime = 0
+let unitSecond: neopixel.Strip = null
+let tenSecond: neopixel.Strip = null
+let unitMinute: neopixel.Strip = null
+let tenMinute: neopixel.Strip = null
+let unitHour: neopixel.Strip = null
+let tenHour: neopixel.Strip = null
+let strip = neopixel.create(DigitalPin.P0, 20, NeoPixelMode.RGB)
+tenHour = strip.range(0, 2)
+unitHour = strip.range(3, 4)
+tenMinute = strip.range(8, 3)
+unitMinute = strip.range(11, 4)
+tenSecond = strip.range(15, 3)
+unitSecond = strip.range(18, 4)
+strip.showColor(neopixel.colors(NeoPixelColors.Red))
+basic.pause(2000)
+strip.clear()
+DS3231.dateTime(
+2022,
+9,
+22,
+5,
+1,
+25,
+10
+)
+basic.forever(function () {
+    getTenHourTime()
+    getUnitHourTime()
+    getTenMinuteTime()
+    getUnitMinuteTime()
+    getTenSecondTime()
+    getUnitSecondTime()
+})
+basic.forever(function () {
+    showTenHour()
+    showUnitHour()
+    showTenMinute()
+    showUnitMinute()
+    showTenSecond()
+    showUnitSecond()
+})
